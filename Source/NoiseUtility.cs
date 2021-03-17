8f8da15100213d693890cb2a9a2c21eee00e17f8
João Pedro Torres <jaopedro05@outlook.com>
2021-03-17 12:49:20 -0300

    Cached soundDef levels.

--- Source/NoiseUtility.cs
+++ Source/NoiseUtility.cs
@@ -9,12 +9,19 @@ namespace KeepItQuiet
 {
     class NoiseUtility
     {
+        private static Dictionary<SoundDef, float> cachedLevels = new Dictionary<SoundDef, float>();
+
         public static float GetSoundLevel(SoundDef soundDef)
         {
             float result = 1f;
-            if (soundDef.subSounds.Any(x => x.volumeRange != null))
+            if (cachedLevels.ContainsKey(soundDef)) result = cachedLevels[soundDef];
+            else
             {
-                result = soundDef.subSounds.Select(x => x.volumeRange.Average).Aggregate((a, b) => a > b ? a : b);
+                if (soundDef.subSounds.Any(x => x.volumeRange != null))
+                {
+                    result = soundDef.subSounds.Select(x => x.volumeRange.Average).Aggregate((a, b) => a > b ? a : b);
+                }
+                cachedLevels.Add(soundDef, result);
             }
             return result;
         }
